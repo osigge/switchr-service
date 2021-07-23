673156b5142fbacfffdfbeff617a28736ca460cc
Oliver Sigge <osigge@mac.com>
2021-07-23 19:25:01 +0200

    feat: initial commit

---
+++ src/main/java/de/osigge/switchrservice/SwitchrServiceApplication.java
@@ -0,0 +1,14 @@
+package de.osigge.switchrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchrServiceApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/de/osigge/switchrservice/SwitchrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.osigge.switchrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
